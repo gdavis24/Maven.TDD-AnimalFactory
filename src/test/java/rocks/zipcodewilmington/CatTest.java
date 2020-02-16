f47056de89ed9c0dd33dcc8164c60fd57ba7ffb3
Gregory Davis <gdavis@zipcodes-MacBook-Pro.local>
2020-02-16 12:57:00 -0500

    half cat tests running

--- src/test/java/rocks/zipcodewilmington/CatTest.java
+++ src/test/java/rocks/zipcodewilmington/CatTest.java
@@ -3,6 +3,7 @@ package rocks.zipcodewilmington;
 import org.junit.Assert;
 import org.junit.Test;
 import rocks.zipcodewilmington.animals.Cat;
+import rocks.zipcodewilmington.animals.Dog;
 
 import java.util.Date;
 
@@ -40,4 +41,48 @@ public class CatTest {
         Assert.assertEquals(givenId, retrievedId);
     }
 
+    @Test
+    public void testNameTest() {
+        // Given (a name exists and a cat exists)
+        Cat newCat = new Cat(null, null, null);
+        String givenName = "Zula";
+
+        // When (a cat's name is set to the given name)
+        newCat.setName(givenName);
+
+        // Then (we expect to get the given name from the cat)
+        String catName = newCat.getName();
+        Assert.assertEquals(catName, givenName);
+    }
+    @Test
+    public void testBirthDate() {
+
+        Cat newCat = new Cat(null, null, null);
+        Date birthDate = new Date();
+
+
+        newCat.setBirthDate(birthDate);
+        Date expected = newCat.getBirthDate();
+
+
+        Date actual = newCat.getBirthDate();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSpeak() {
+        Cat newCat = new Cat(null, null, null);
+        String expected = "meow!";
+        String actual = newCat.speak();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testEatFood() {
+        Cat newCat = new Cat(null, null, null);
+        newCat.eat(new Food());
+        Integer expected = 2;
+        Integer actual = newCat.getNumberOfMealsEaten();
+        Assert.assertEquals(expected, actual);
+    }
 }
